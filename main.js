23b11a5ba704107974a6a5e564c4a2ec58f6f8f0
gangancuicui <liangsujunliang@gmail.com>
2023-12-08 09:13:20 +0800

    删除冗余代码
    Signed-off-by: gangancuicui <liangsujunliang@gmail.com>

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 import App from './App'
-import { $http } from '@escook/request-miniprogram'
+// import { $http } from '@escook/request-miniprogram'
 import store from './store/store.js'
 
 
 
-uni.$http = $http
-$http.baseUrl='https://api-hmugo-web.itheima.net'
+// uni.$http = $http
 
 // 请求开始之前做一些事情
-$http.beforeRequest = function (options) {
-  uni.showLoading({
-    title: '数据加载中...',
-  })
-}
+// $http.beforeRequest = function (options) {
+//   uni.showLoading({
+//     title: '数据加载中...',
+//   })
+// }
 
 // 请求完成之后做一些事情
-$http.afterRequest = function () {
-  uni.hideLoading()
-}
+// $http.afterRequest = function () {
+//   uni.hideLoading()
+// }
 
 uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
   uni.showToast({
